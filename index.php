bcf7ae681891badd36b83c0b0cc538968549bf34
Rena3001 <verdiyevarana@gmail.com>
2023-11-04 19:03:56 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// class Db
+// {
+//     public $name;
+//     public $surname;
+//     public $email;
+//     public $password;
+    
+  
+// }
+// class AnotherClass extends Db
+// {
+//     public function __construct($name,$surname,$email,$password)
+//     {
+        
+//     }
+// }
+// $db=new Db("Murad","Agamedov","agamedov@mail.ru","123");
+// echo $db->name;
+?>
